dc70098d24c56786da105f8ceeae768df57a3eeb
djballowe <djballowe@gmail.com>
2025-01-07 17:22:01 -0800

    merge

--- init.lua
+++ init.lua
@@ -56,13 +56,13 @@ require('lazy').setup({
   {
     'NeogitOrg/neogit',
     dependencies = {
-      'nvim-lua/plenary.nvim', -- required
+      'nvim-lua/plenary.nvim',  -- required
       'sindrets/diffview.nvim', -- optional - Diff integration
 
       -- Only one of these is needed.
       'nvim-telescope/telescope.nvim', -- optional
-      'ibhagwan/fzf-lua', -- optional
-      'echasnovski/mini.pick', -- optional
+      'ibhagwan/fzf-lua',              -- optional
+      'echasnovski/mini.pick',         -- optional
     },
     config = true,
   },
@@ -84,7 +84,7 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
+  { 'folke/which-key.nvim',  opts = {} },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -42,7 +42,7 @@ return {
     'stevearc/conform.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
-      local conform = require 'conform'
+      local conform = require('conform')
       conform.setup {
         formatters_by_ft = {
           javascript = { 'prettier' },

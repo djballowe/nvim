367d4b094b1ad8ed1c2a4e22449a2393a94f3bba
David Ballowe <101905433+djballowe@users.noreply.github.com>
2026-08-31 09:21:03 -0700

    Add Astro formatting support

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -16,6 +16,7 @@ return {
           yaml = { 'prettier' },
           markdown = { 'prettier' },
           graphql = { 'prettier' },
+          astro = { 'prettier' },
           lua = { 'stylua' },
           c = { 'clang-format' },
           cpp = { 'clang-format' },
